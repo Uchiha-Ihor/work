3d726714e0aa1c6a9598985de795fafa2bcb3a71
Ihor_Uchiha <ihorychiha@gmail.com>
2022-08-23 11:01:01 +0300

    Create Client and Server

--- main.c
+++ main.c
@@ -9,12 +9,12 @@ int broken(void){
 	char local[5];
 	
 	dyn = malloc(5);
-	strcpy(dyn, "12345");
+	strcpy(dyn, "7734");
 	printf("1:%s\n", dyn);
 	free(dyn);
 	
 	dyn = malloc(5);
-	strcpy(dyn, "12345678");
+	strcpy(dyn, "1234");
 	printf("2: %s\n", dyn);
 	
 	/* Пройти перед началом выделенного с помощью malloc локального буфера */
@@ -23,13 +23,13 @@ int broken(void){
 	/* обратите внимание, что указатель не освобожден! */
 
 	/* Теперь двинуться после переменной local */
-	strcpy(local, "12345");
+	strcpy(local, "1234");
 	printf ("4: %s\n", local);
 	local[-1] = '\0';
 	printf("5: %s\n", local);
 
 	/* Наконец, атаковать пространство данных global */
-	strcpy(global, "12345");
+	strcpy(global, "1234");
 	printf ("6: %s\n", global);
 
 	/* И записать поверх пространства перед буфером global */
